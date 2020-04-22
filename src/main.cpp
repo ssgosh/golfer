9ba92e10e592ea7465b911682f48e72f0a108c12
Sabyasachi Ghosh <ssgosh@gmail.com>
2020-04-22 19:02:15 +0530

    Fix build issues with cmake

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-#include "log4cpp/Category.hh"
-#include "log4cpp/Appender.hh"
-#include "log4cpp/Layout.hh"
-#include "log4cpp/BasicLayout.hh"
-#include "log4cpp/Priority.hh"
-#include "log4cpp/Ostreamappender.hh"
-#include "log4cpp/FileAppender.hh"
+#include <log4cpp/Category.hh>
+#include <log4cpp/Appender.hh>
+#include <log4cpp/Layout.hh>
+#include <log4cpp/BasicLayout.hh>
+#include <log4cpp/Priority.hh>
+#include <log4cpp/OstreamAppender.hh>
+#include <log4cpp/FileAppender.hh>
 #include <boost/program_options.hpp>
 #include "sgp.hh"
 #include "tabu.hh"
