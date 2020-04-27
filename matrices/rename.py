fba7cb665621934de2ad3e72be31cd31a6ff1f7e
Sabyasachi Ghosh <ssgosh@gmail.com>
2020-04-27 18:56:25 +0530

    Rename newly added social_golfer to kirkman

--- matrices/rename.py
+++ matrices/rename.py
@@ -1,7 +1,7 @@
 import os
 
 for fname in os.listdir("."):
-  if fname.startswith("optimized_M_"):
+  if fname.find("social_golfer") != -1:
     newname = fname.replace("social_golfer", "kirkman")
     print(newname)
     os.rename(fname, newname)
